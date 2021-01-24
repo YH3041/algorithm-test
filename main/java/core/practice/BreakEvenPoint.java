e6190044446a229eaad48da1b3a20b45d73c567a
YH3041 <mmj2rv9@gmail.com>
2021-01-24 23:28:01 +0900

    feat: 손익분기점 계산 기
    고정비용, 가변비용, 제품가격의 주어졌을 떄 손인분기점을 구하는 프로그램이다.

---
+++ main/java/core/practice/BreakEvenPoint.java
@@ -0,0 +1,22 @@
+package core.practice;
+
+// 노트북 판매 대수에 상관없이 매년 임대료, 재산세, 보험료, 급여 등 A만원의 고정 비용
+// 한 대의 노트북을 생산하는 데에는 재료비와 인건비 등 총 B만원의 가변 비용
+// 노트북 가격이 C만원으로 책정
+// 일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다.
+// 최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
+// A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
+
+import java.util.Scanner;
+
+public class BreakEvenPoint {
+
+    private int FC;
+    private int materialCost;
+    private int laptopPrice;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}

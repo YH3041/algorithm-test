11d4bd3d0cea6d7fd79c15e38bef49d0bdb8d161
YH3041 <mmj2rv9@gmail.com>
2021-01-25 15:06:19 +0900

    feat: 손익분기점 계산기 수정
    손익분기점 프로그램 기능 수정

--- main/java/core/practice/BreakEvenPoint.java
+++ main/java/core/practice/BreakEvenPoint.java
@@ -6,17 +6,25 @@ package core.practice;
 // 일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다.
 // 최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
 // A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
+// 최초로 이익이 발생하는 판매량을 출력한다.
 
 import java.util.Scanner;
 
 public class BreakEvenPoint {
-
-    private int FC;
-    private int materialCost;
-    private int laptopPrice;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if(B >= C) {
+            System.out.println(-1);
+        } else {
+            System.out.println((A /( C - B)) + 1);
+        }
+
     }
+
 }
+

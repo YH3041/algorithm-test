c9eae6d9b88533a62466c6e869296b2345862c49
YH3041 <mmj2rv9@gmail.com>
2021-02-01 20:43:08 +0900

    feat: 직사각형 네번째 점을 찾는 기능 구현
    3개의 점이 주어졌을 때 4번째 점을 구하는 찾아서 출력한다.

---
+++ main/java/core/practice/FindFourthPoint.java
@@ -0,0 +1,47 @@
+package core.practice;
+
+import java.util.Scanner;
+
+// 세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
+
+public class FindFourthPoint {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        int x4 = 0;
+        int y4 = 0;
+
+        if(x1 == x2) {
+            x4 = x3;
+        } else if(x2 == x3) {
+            x4 = x1;
+        } else if(x1 == x3) {
+            x4 = x2;
+        } else {
+            System.out.println(-1);
+        }
+
+        if(y1 == y2) {
+            y4 = y3;
+        } else if(y2 == y3) {
+            y4 = y1;
+        } else if(y1 == y3) {
+            y4 = y2;
+        } else {
+            System.out.println(-1);
+        }
+
+        System.out.println(x4 + " " + y4);
+    }
+}

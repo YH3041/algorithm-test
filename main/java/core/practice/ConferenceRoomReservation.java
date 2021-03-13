32d470b17641160d1d05aa7efb76f5260f996afc
YH3041 <mmj2rv9@gmail.com>
2021-03-13 16:35:08 +0900

    feat: 회의실 예약
    각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자.

---
+++ main/java/core/practice/ConferenceRoomReservation.java
@@ -0,0 +1,43 @@
+package core.practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class ConferenceRoomReservation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] start, int[] end) {
+                if (start[1] == end[1]) {
+                    // 종료 시간이 같을경우엔 시작시간으로 정렬
+                    return Integer.compare(start[0], end[0]);
+                }
+
+                return Integer.compare(start[1], end[1]);
+            }
+        });
+
+        int count = 0;
+        int end = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i][0] >= end) {
+                end = arr[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
